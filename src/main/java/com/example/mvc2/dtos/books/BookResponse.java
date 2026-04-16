e5259dea25e32057522eab533cae350c5c1279b7
Daria Kozina <kozina.flowers@yandex.ru>
2026-04-16 18:09:02 +0300

    in progress

---
+++ src/main/java/com/example/mvc2/dtos/books/BookResponse.java
@@ -0,0 +1,12 @@
+package com.example.mvc2.dtos.books;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class BookResponse {
+    private Long id;
+}
