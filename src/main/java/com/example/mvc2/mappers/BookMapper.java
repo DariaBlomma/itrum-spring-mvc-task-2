5dbc3a2ba706200af97127a16678fc7d124adf56
daria <kozina.flowers@yandex.ru>
2026-04-16 20:19:42 +0300

    prepared mappers

---
+++ src/main/java/com/example/mvc2/mappers/BookMapper.java
@@ -0,0 +1,26 @@
+package com.example.mvc2.mappers;
+
+import com.example.mvc2.dtos.books.BookRequest;
+import com.example.mvc2.dtos.books.BookResponse;
+import com.example.mvc2.entities.Author;
+import com.example.mvc2.entities.Book;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+public interface BookMapper {
+    Book toEntity(BookRequest request);
+
+    @Mapping(source = "authors", target = "authorIds")
+    BookResponse toResponse(Book book);
+
+    default Set<Long> mapAuthorsToIds(Set<Author> authors) {
+        if (authors == null) return Set.of();
+        return authors.stream().map(Author::getId).collect(Collectors.toUnmodifiableSet());
+    }
+
+    void update(BookRequest request, @MappingTarget Book user);
+}
